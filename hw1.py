4aeb7c657eab2c368a2063d02f5fa60f1b58b0dd
rajat <rrathee2@illinois.edu>
2018-09-19 21:40:57 -0500

    start

---
+++ hw1.py
@@ -0,0 +1,27 @@
+#
+# CS 196 Data Hackerspace
+# Assignment 1: Data Parsing and NumPy
+# Due September 24th, 2018
+#
+
+import json
+import csv
+import numpy as np
+
+def histogram_times(filename):
+    pass
+
+def weigh_pokemons(filename, weight):
+    pass
+
+def single_type_candy_count(filename):
+    pass
+
+def reflections_and_projections(points):
+    pass
+
+def normalize(image):
+    pass
+
+def sigmoid_normalize(image):
+    pass
